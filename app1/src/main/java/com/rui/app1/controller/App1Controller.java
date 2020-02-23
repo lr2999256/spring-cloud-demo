22fecefe7b206f85a63aa67ed2b3fc9227bfd2ee
ruipost <707098275@qq.com>
2020-02-23 14:53:17 +0800

    增加配置服务文件

--- app1/src/main/java/com/rui/app1/controller/App1Controller.java
+++ app1/src/main/java/com/rui/app1/controller/App1Controller.java
@@ -2,7 +2,9 @@ package com.rui.app1.controller;
 
 import com.rui.app1.feign.FeignApp2Service;
 import com.rui.app1.service.App1Service;
-import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -13,7 +15,6 @@ import javax.annotation.Resource;
  * @Time : 9:57
  **/
 @RestController
-@Log
 public class App1Controller {
 
     @Resource
@@ -22,17 +23,28 @@ public class App1Controller {
     @Resource
     private FeignApp2Service feignClient2Service;
 
+    @Value("${my.name2}")
+    private String myName;
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(App1Controller.class);
+
     @GetMapping("/app1/hello/{name}")
     public String app2SayHello(@PathVariable("name") String name){
-        log.info("Nice to meet you app2 !");
-        log.info(feignClient2Service.toApp2SayHello(name));
+        LOGGER.info("Nice to meet you app2 !");
+        LOGGER.info(feignClient2Service.toApp2SayHello(name));
+        return "success";
+    }
+
+    @GetMapping("/config/test")
+    public String configTest(){
+        LOGGER.info("config test!,{}",myName);
         return "success";
     }
 
     @PostMapping("/app1/bye")
     public String app1SayBye(@RequestParam("name") String name){
-        log.info("app1 is tired too.");
-        log.info(app1Service.app1SayBye(name));
+        LOGGER.info("app1 is tired too.");
+        LOGGER.info(app1Service.app1SayBye(name));
         return "success";
     }
 }
--- app2/src/main/java/com/rui/app2/controller/App2Controller.java
+++ app2/src/main/java/com/rui/app2/controller/App2Controller.java
@@ -2,7 +2,8 @@ package com.rui.app2.controller;
 
 import com.rui.app2.feign.FeignApp1Service;
 import com.rui.app2.service.App2Service;
-import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -13,7 +14,6 @@ import javax.annotation.Resource;
  * @Time : 9:57
  **/
 @RestController
-@Log
 public class App2Controller {
 
     @Resource
@@ -22,18 +22,20 @@ public class App2Controller {
     @Resource
     private FeignApp1Service feignClient1Service;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(App2Controller.class);
+
     @GetMapping("/app2/hello/{name}")
     public String app2Hello(@PathVariable("name") String name){
-        log.info("app2 meet "+name+" for the first time");
-        log.info(app2Service.app2SayHello(name));
+        LOGGER.info("app2 meet "+name+" for the first time");
+        LOGGER.info(app2Service.app2SayHello(name));
         return "success";
     }
 
     @PostMapping("/app2/bye")
     public String app2Bye(String name){
-        log.info("app2 do something just he like");
-        log.info("app2 is tired");
-        log.info(feignClient1Service.toApp1SayBye(name));
+        LOGGER.info("app2 do something just he like");
+        LOGGER.info("app2 is tired");
+        LOGGER.info(feignClient1Service.toApp1SayBye(name));
         return "successs";
     }
 }
